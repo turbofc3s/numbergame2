d2de57b14cd0f0d221eaa3383591336413772b67
jeffrey burke <burkejeffrey56@yahoo.com>
2022-04-26 20:34:03 -0400

    second commit

---
+++ style.js
@@ -0,0 +1,9 @@
+
+function randomNum () {
+	var  ranNum = [Math.floor(Math.random() * 100)]		
+		console.log(ranNum);
+
+
+formSubmit.addEventListener('click', function (event) {
+event.preventDefault();
+});	
